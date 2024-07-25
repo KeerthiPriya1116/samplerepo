97991847cf5b92730472b42a640cd9e4f99784b0
pkeer <pkeer@DESKTOP-BUK2US3.mshome.net>
2024-07-25 16:19:04 +0530

    Update

---
+++ SampleProject/src/samplepro/Sample1.java
@@ -0,0 +1,16 @@
+package samplepro;
+
+import java.util.Date;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Hello..Github");
+         String time = new Date().toString();
+         System.out.println("Todays date:"+time+"");
+         System.out.println("hellooo.hii....");
+         System.out.println("Update");
+	}
+
+}
