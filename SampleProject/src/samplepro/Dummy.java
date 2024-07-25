6b5b3dabbdce8bd57b53feb0dbe1b27017d7baa1
pkeer <pkeer@DESKTOP-BUK2US3.mshome.net>
2024-07-25 17:10:31 +0530

    updated new class

---
+++ SampleProject/src/samplepro/Dummy.java
@@ -0,0 +1,11 @@
+package samplepro;
+
+public class Dummy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Hii..Hello");
+     System.out.println("How are you?");
+	}
+
+}
